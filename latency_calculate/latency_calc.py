7f41a284e633d265610461b527f2d444f58576a5
Anirudh-S-Kumar <anirudh.skumar.03@gmail.com>
2023-09-08 03:14:16 +0530

    Completed Code for latency_calc, generated requirements.txt

--- latency_calculate/latency_calc.py
+++ latency_calculate/latency_calc.py
@@ -1,7 +1,10 @@
 import pyshark
-
+import statistics
+from datetime import datetime
 # Open the pcapng file for reading
 cap = pyshark.FileCapture('latency_cap_server.pcapng' )
+
+diffs = []
 
 # Loop through the packets in the file
 for pkt in cap:
@@ -9,4 +12,20 @@ for pkt in cap:
     if 'ip' in pkt and 'eth' in pkt:
         pkt_bytes = (pkt.eth.trailer)
         sniff_time = pkt.sniff_time
-        print(pkt_bytes, sniff_time)
+        pkt_bytes = pkt_bytes.split(':')
+        if pkt_bytes:
+            pkt_bytes[:4] = pkt_bytes[:4][::-1]
+            pkt_bytes[8:] = pkt_bytes[8:][::-1]
+            seconds = int("".join(pkt_bytes[:4]), 16)
+            nanoseconds = int("".join(pkt_bytes[8:]), 16)
+            obj = datetime.fromtimestamp(seconds + nanoseconds / 1e9)
+            diffs.append((obj - sniff_time).total_seconds())
+
+diffs.sort()
+
+print("Mean: ", statistics.mean(diffs))
+print("Median: ", statistics.median(diffs))
+print("Standard Deviation: ", statistics.stdev(diffs))
+print("Variance: ", statistics.variance(diffs))
+print("Max: ", diffs[-1])
+print("Min: ", diffs[0])
