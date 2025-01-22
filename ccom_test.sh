09b8387da2ad29ffe7bd0ba262e8efd4bc83de57
Prateek Kumar <prateek21081@iiitd.ac.in>
2025-01-22 11:14:46 +0530

    fix test audio recording  delay

--- ccom_test.sh
+++ ccom_test.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -x
 
-for i in {1..3}; do 
-	timeout 30 ssh afterchange@192.168.226.175 -C "timeout 30 tshark -w ~/sv_auto/pcap/$i.pcap 'tcp and host 192.168.226.176'" &
-	timeout 30 ssh afterchange@192.168.226.175 -C "timeout 30 parec -d 'alsa_output.pci-0000_08_00.4.analog-stereo.monitor' --latency-msec=20 --rate=16000 --file-format=wav ~/sv_auto/audio/$i.wav" &
-	timeout 30 ssh afterchange@192.168.226.175 -C "timeout 30 cat /tmp/julius.raw > /tmp/virtmic" &
+for i in {1..10}; do 
+	ts=$(date -u +%s)
+	timeout 32 ssh afterchange@192.168.226.175 -C "timeout 32 tshark -w ~/sv_auto/pcap/$ts.pcap 'udp and host 192.168.226.176'" &
+	timeout 32 ssh afterchange@192.168.226.175 -C "timeout 32 parec -d 'alsa_output.pci-0000_08_00.4.analog-stereo.monitor' --latency-msec=20 --rate=16000 --file-format=wav ~/sv_auto/audio/$ts.wav" &
+	timeout 32 tshark -w ~/sv_auto/pcap/$ts.pcap 'udp and host 192.168.226.175' &
+	timeout 32 parec -d 'alsa_output.pci-0000_00_1f.3.analog-stereo.monitor' --latency-msec=20 --rate=16000 --file-format=wav ~/sv_auto/audio/$ts.wav & 
 
-	timeout 30 tshark -w ~/sv_auto/pcap/$i.pcap 'tcp and host 192.168.226.175' &
-	timeout 30 parec -d 'alsa_output.pci-0000_00_1f.3.analog-stereo.monitor' --latency-msec=20 --rate=16000 --file-format=wav ~/sv_auto/audio/$i.wav & 
-	timeout 30 cat /tmp/julius.raw > /tmp/virtmic &
+	sleep 1
+	timeout 32 ssh afterchange@192.168.226.175 -C "timeout 32 cat /tmp/julius.raw > /tmp/virtmic" &
+	timeout 32 cat /tmp/julius.raw > /tmp/virtmic &
+
 	wait
 
 	sleep 5
